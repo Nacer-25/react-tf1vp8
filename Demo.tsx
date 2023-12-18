9dd7ee00cb6ca0e035192f9e4173e018fe7ee776
Nacer-25 <151617532+Nacer-25@users.noreply.github.com>
2023-12-18 21:34:24 +0100

    Update Demo.tsx
    last commit

--- Demo.tsx
+++ Demo.tsx
@@ -5,8 +5,6 @@ const data = [
   { id: 0, value: 36, label: '1.0' },
   { id: 1, value: 17, label: '2.0' },
   { id: 2, value: 19, label: '2.5' },
-  { id: 3, value: 21, label: '4.0' },
-  { id: 4, value: 5, label: '5.0' },
 ];
 
 export default function PieActiveArc() {
